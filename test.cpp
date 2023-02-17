a4308119d9102d42ad488e1e10c3d46b23ee55cf
Michael Belousov <mike.belousov@bentley.com>
2023-02-17 11:37:31 -0500

    wip optimized unicode trim

--- test.cpp
+++ test.cpp
@@ -1,8 +1,10 @@
 
 #include <string>
 #include <stdlib.h>
+#include <locale>
 #include <unicode/unistr.h>
 #include <unicode/ustream.h>
+#include <unicode/bytestream.h>
 #include <regex>
 #include <iostream>
 #include <exception>
@@ -27,16 +29,43 @@ std::string& BentleyAsciiTrim (std::string& s)
     return s;
     }
 
-std::string& IcuTrim (std::string& s)
+// doesn't work, since this is a utf8 string, need to iterate over it by code points
+std::string& BentleyLocaleTrim (std::string& s)
     {
-    auto us = icu::UnicodeString::fromUTF8(s.c_str());
-    us.trim();
-    std::cout << "trimmed: '" << us << "'" << std::endl;
-    s.clear();
-    us.toUTF8String(s);
+    if (s.empty ())
+        return s;
+
+    size_t firstNonSpaceIdx = 0;
+    for (; (firstNonSpaceIdx < s.length ()) && isspace (s.at (firstNonSpaceIdx), std::locale("en_US.utf8")); ++firstNonSpaceIdx)
+        ;
+
+    size_t lastNonSpaceIdx = s.length () - 1;
+    for (; (lastNonSpaceIdx > firstNonSpaceIdx) && isspace (s.at (lastNonSpaceIdx), std::locale("en_US.utf8")); --lastNonSpaceIdx)
+        ;
+
+    s.erase ((s.begin () + lastNonSpaceIdx + 1), s.end ());
+    s.erase (s.begin (), (s.begin () + firstNonSpaceIdx));
     return s;
     }
 
+std::string& IcuTrim (std::string& s)
+    {
+    icu::UnicodeString us (s.data(), s.size(), US_INV);
+    //auto us = icu::UnicodeString::fromUTF8(s.c_str());
+    //s.clear();
+    //us.toUTF8String(s);
+    std::cout << "untrimmed: '" << us << "'" << std::endl;
+    us.trim();
+    std::cout << "trimmed: '" << us << "'" << std::endl;
+    //auto sink = icu::CheckedArrayByteSink(s.data(), s.capacity());
+    //us.toUTF8(sink);
+    // in theory if the utf8 is normalized trimming could increase the size of the string and break
+    // out the buffer, no? Perhaps better to use StringByteSink
+    //s.resize(sink.NumberOfBytesAppended());
+    return s;
+    }
+
+/*
 std::regex unicode_spaces{"[\r\n\t\f\v \u00a0\u1680\u2000-\u200a\u2028\u2029\u202f\u205f\u3000\ufeff]"};
 
 std::string& RegexTrim (std::string& s)
@@ -46,31 +75,35 @@ std::string& RegexTrim (std::string& s)
     s = out;
     return s;
     }
+*/
+
+#define NBSP "\u00a0"
+#define EMSPC "\u2003"
 
 BENCHMARK(TrimTest, std::string&(*)(std::string&))
 {
   SETUP_BENCHMARK(
     const auto trim_func = GET_ARGUMENT(0);
     std::vector<std::string> utf8_strings {
-      "test\u00a0",
-      "\u00a0te\u00a0st\u00a0",
-      "\u00a0te\u00a0st\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0",
+      "test" EMSPC,
+      EMSPC "te" EMSPC "st" EMSPC,
+      EMSPC "te" EMSPC "st" EMSPC EMSPC EMSPC EMSPC EMSPC EMSPC EMSPC EMSPC,
       "      te      st                                                      ",
-      "this is an debateably long string without any non-ascii characters",
+      "this is a debateably long string without any non-ascii characters",
     };
     std::vector<std::string> ascii_trimmed_strings {
-      "test\u00a0",
-      "\u00a0te\u00a0st\u00a0",
-      "\u00a0te\u00a0st\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0",
+      "test" EMSPC,
+      EMSPC "te" EMSPC "st" EMSPC,
+      EMSPC "te" EMSPC "st" EMSPC EMSPC EMSPC EMSPC EMSPC EMSPC EMSPC EMSPC,
       "te      st",
-      "this is an debateably long string without any non-ascii characters",
+      "this is a debateably long string without any non-ascii characters",
     };
     std::vector<std::string> utf8_trimmed_strings {
       "test",
-      "te\u00a0st",
-      "te\u00a0st",
+      "te" EMSPC "st",
+      "te" EMSPC "st",
       "te      st",
-      "this is an debateably long string without any non-ascii characters",
+      "this is a debateably long string without any non-ascii characters",
     };
   )
 
@@ -90,9 +123,10 @@ BENCHMARK(TrimTest, std::string&(*)(std::string&))
 }
 
 INVOKE_BENCHMARK_FOR_EACH(TrimTest,
-  ("BentleyAsciiTrim", BentleyAsciiTrim),
-  ("IcuTrim", IcuTrim),
-  ("RegexTrim", RegexTrim)
+  //("BentleyAsciiTrim", BentleyAsciiTrim),
+  //("BentleyLocaleTrim", BentleyLocaleTrim),
+  ("IcuTrim", IcuTrim)
+  //("RegexTrim", RegexTrim)
 )
 
 CRITERION_BENCHMARK_MAIN()
